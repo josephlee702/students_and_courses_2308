7d821f7cd5522268135bed526d2a743173813ba6
Joseph Lee <jhjlee702@gmail.com>
2023-09-06 09:32:27 -0600

    Create student

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
 class Student
+  attr_reader :name, :age, :scores
   
+  def initialize(attributes)
+    @name = attributes[:name]
+    @age = attributes[:age]
+    @scores = []
+  end
+
+  def log_score(test_score)
+    @scores << test_score
+  end
+
+  def grade
+    @scores.sum.to_f/2
+  end
 end
